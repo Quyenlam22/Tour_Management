db83ec81800465ab337780221fd9ba4814160a08
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-03-01 23:14:28 +0700

    Create basic route

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
@@ -9,10 +9,22 @@ const port: number | string = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
+
+// Folder Public
+app.use(express.static(`${__dirname}/public`))
+
 app.get("/", (req: Request, res: Response) => {
     res.send(`Trang chủ`);
+});
+
+app.get("/tours", (req: Request, res: Response) => {
+    res.render("client/pages/tours/index", {
+        pageTitle: 'Danh sách tour'
+    });
 })
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-})
+});
