a8f9071b56c3ce7d8cac58529bb28577b8ca405f
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-03-02 15:16:49 +0700

    tour categories

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,10 +1,12 @@
 import {Express} from "express";
 import { tourRoutes } from "./tour.route";
 import { homeRoutes } from "./home.route";
+import { categoryRoutes } from "./category.route";
 
 const clientRoutes = (app: Express): void => {
     app.use(`/`, homeRoutes);
     app.use(`/tours`, tourRoutes);
+    app.use(`/categories`, categoryRoutes);
 };
 
 export default clientRoutes;
