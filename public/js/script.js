9cc1473a71f0f5933e870df18fb3a26657a36d39
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-03-05 09:04:01 +0700

    Add tour to cart

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+// Slider Tour Detail
 var swiperThumb = new Swiper(".imagesThumb", {
   spaceBetween: 10,
   slidesPerView: 4,
@@ -14,3 +15,36 @@ var swiperMain = new Swiper(".imagesMain", {
     swiper: swiperThumb,
   },
 });
+
+// Cart
+const cart = localStorage.getItem("cart");
+if(!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+const formAddToCard = document.querySelector("[form-add-to-card]");
+if(formAddToCard) {
+  formAddToCard.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCard.getAttribute("tour-id"));
+    
+    if(quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+      
+      const indexExist = cart.findIndex(item => item.tourId == tourId);
+
+      if(indexExist == -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        });
+      }
+      else{
+        cart[indexExist].quantity = cart[indexExist].quantity + quantity;
+      }
+      
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+}
