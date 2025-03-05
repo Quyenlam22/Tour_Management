6ed8c7aa38e4b6be77e05547859425744b667f20
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-03-05 08:50:58 +0700

    Display image slider

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+var swiperThumb = new Swiper(".imagesThumb", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiperMain = new Swiper(".imagesMain", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiperThumb,
+  },
+});
